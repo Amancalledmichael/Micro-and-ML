408a8f03d2b9a8c08940375b497c9f8db303a1bf
Amancalledmichael <104633732+Amancalledmichael@users.noreply.github.com>
2022-04-29 16:32:10 +0200

    Add files via upload

---
+++ 2.2_Git.R
@@ -0,0 +1,42 @@
+setwd("M:/Kurse/Micro and ML/Tutorial")
+
+labsupp <- read.csv(file = 'mroz_wool.csv')
+View(labsupp)
+
+#OLS
+OLS<-lm(hours ~ kidslt6 + kidsge6 + age + educ + city + exper + expersq, data = labsupp)
+summary(OLS)
+
+
+#1)
+library(AER)
+tobit<-tobit(hours ~ kidslt6 + kidsge6 + age + educ + city + exper + expersq, data = labsupp)
+summary(tobit)
+tobittable <- coef(summary(tobit))
+
+
+#2)
+library(truncreg)
+trunc <- truncreg(hours ~ kidslt6 + kidsge6 + age + educ + city + exper + expersq, data = labsupp)
+summary(trunc)
+trunctable <- coef(summary(tobit))
+
+
+#3)
+#Wage is highly edogenous to hours. Hours affect wages and vice versa. "bad control" problem. The regression equation can be interpreted as how family care affects labour supply of women 
+
+#4)
+income<-lm(hours ~ faminc, data = labsupp)
+summary(income)
+library(margins)
+margins(income)
+
+incomecond<-lm(hours ~ faminc, data = subset(labsupp, inlf==1))
+summary(incomecond)
+margins(incomecond)
+
+tobitwage<-tobit(hours ~ wage, data = labsupp)
+summary(tobitwage)
+
+OLSwage<-lm(hours ~ wage, data=labsupp)
+margins(OLSwage)
